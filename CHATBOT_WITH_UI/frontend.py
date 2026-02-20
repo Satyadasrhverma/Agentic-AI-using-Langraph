93ea202c217f7ec6fd813f30234a4b7b1e236970
Satyadarsh verma <satya12darsh@gmail.com>
2026-02-20 16:46:26 +0530

    some changes , invoke to stream

--- CHATBOT_WITH_UI/frontend.py
+++ CHATBOT_WITH_UI/frontend.py
@@ -19,12 +19,30 @@ for message in st.session_state['message_history']:
 user_input = st.chat_input("Type your messages.....")
 
 if user_input:
-    st.session_state['message_history'].append({"role" : "user" , "content" : user_input})
-    with st.chat_message("user"):
-        st.text(user_input)
+    
+    st.session_state["message_history"].append(
+        {"role": "user", "content": user_input}
+    )
 
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config={'configurable' : {'thread_id': st.session_state.thread_id}})
-    ai_message = response['messages'][-1].content
-    st.session_state['message_history'].append({"role" : "user" , "content" : ai_message})
-    with st.chat_message('assistant'):
-        st.text(ai_message)    
+    with st.chat_message("user"):
+        st.write(user_input)
+
+    
+    full_response = ""
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+
+        for message_chunk, metadata in chatbot.stream(
+            {"messages": [HumanMessage(content=user_input)]},
+            config={"configurable": {"thread_id": st.session_state.thread_id}},
+            stream_mode="messages"
+        ):
+            if message_chunk.content:
+                full_response += message_chunk.content
+                message_placeholder.markdown(full_response)
+
+    
+    st.session_state["message_history"].append(
+        {"role": "assistant", "content": full_response}
+    )
